1c8f46680e5b478502791bc9b0c0155e9f578f86
antonknk <antonkoennekemail@yahoo.de>
2023-06-05 20:29:50 +0200

    fix terrible length vs. lengths bug

--- R/anygender.R
+++ R/anygender.R
@@ -21,7 +21,7 @@ any_gender <- function(text){
         stringr::str_squish() %>%
         stringr::str_extract_all("(\\w+innen\\s+(und|&)\\s+\\w+|\\w+\\s+(und|&)\\s+\\w+innen)")
 
-    n_gendered_pair <- length(purrr::pluck(matches_pair, 1))
+    n_gendered_pair <- lengths(matches_pair)
 
     n_words <- text %>%
         str_count(boundary("word"))
@@ -37,7 +37,7 @@ any_gender <- function(text){
         stringr::str_squish() %>%
         stringr::str_extract_all("\\S+([:punct:]i|I)nnen\\b")
 
-    n_gendered_symbol <- length(purrr::pluck(matches_symbol, 1))
+    n_gendered_symbol <- lengths(matches_symbol)
 
     percent_gendered_symbol <- n_gendered_symbol / n_words
 
@@ -50,33 +50,34 @@ any_gender <- function(text){
         stringr::str_squish() %>%
         stringr::str_extract_all(neutral_forms)
 
-    n_gendered_neutral <- length(purrr::pluck(matches_neutral, 1))
+    n_gendered_neutral <- lengths(matches_neutral)
 
     percent_gendered_neutral <- n_gendered_neutral / n_words
 
     # TOTAL STAT
-    total_gendered_n = sum(c(n_gendered_symbol, n_gendered_pair, n_gendered_neutral), na.rm = T)
+    total_gendered_n = n_gendered_symbol + n_gendered_pair + n_gendered_neutral
     total_gendered_percent = total_gendered_n / n_words
 
 
-    return(
-        tibble(
-            gender_stat = list(list(
-            "matches_pair" = matches_pair,
-            "anygender_pair" = anygender_pair,
-            "n_gendered_pair" = n_gendered_pair,
-            "n_words" = n_words,
-            "percent_gendered_pair" = percent_gendered_pair,
-            "matches_symbol" = matches_symbol,
-            "anygender_symbol" = anygender_symbol,
-            "n_gendered_symbol" = n_gendered_symbol,
-            "percent_gendered_symbol" = percent_gendered_symbol,
-            "matches_neutral" = matches_neutral,
-            "anygender_neutral" = anygender_neutral,
-            "n_gendered_neutral" = n_gendered_neutral,
-            "percent_gendered_neutral" = percent_gendered_neutral,
-            "total_gendered_n" = total_gendered_n,
-            "total_gendered_percent" = total_gendered_percent
-        )))
+    return(tibble::tibble(
+        "matches_pair" = matches_pair,
+        "matches_symbol" = matches_symbol,
+        "matches_neutral" = matches_neutral,
+        "anygender_pair" = anygender_pair,
+        "anygender_symbol" = anygender_symbol,
+        "anygender_neutral" = anygender_neutral,
+        "n_gendered_pair" = n_gendered_pair,
+        "n_gendered_symbol" = n_gendered_symbol,
+        "n_gendered_neutral" = n_gendered_neutral,
+        "n_words" = n_words,
+        "percent_gendered_pair" = percent_gendered_pair,
+        "percent_gendered_symbol" = percent_gendered_symbol,
+        "percent_gendered_neutral" = percent_gendered_neutral,
+        "total_gendered_n" = total_gendered_n,
+        "total_gendered_percent" = total_gendered_percent
+    )
     )
 }
+
+
+
